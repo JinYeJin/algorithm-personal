59cfa880489af029bed7c948ecf06b6bdaf8c61e
Yejin Jin <yeen0606@gmail.com>
2020-03-17 09:15:58 +0900

    modify 5650

--- SWEA/4/5650_.cpp
+++ SWEA/4/5650_.cpp
@@ -4,31 +4,57 @@
     진예진
 */
 
-#include<iostream>
-#include<vector>
-#include<utility>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
 
 using namespace std;
 
 int N;
-int current;
-char direction;
-vector<vector<int>> pinball(100, vector<int>(100, 0));
-vector<pair<int, int>> wormhole(5, {0, 0});
+int current, direction, answer, temp_answer, start_x, start_y;
+bool start_flag;
+vector<vector<int>> pinball(100, vector<int>(100, 0)); 
+ // right: 0, left: 1, up: 2, down: 3
+vector<vector<int>> rule = {{1, 2, 3, 0}, {1, 3, 0, 2},
+                            {3, 0, 1, 2}, {2, 0, 3, 1},
+                            {1, 0, 3, 2}};
+// right, left, up, donw
+vector<int> move_x = {1, -1, 0, 0};
+vector<int> move_y = {0, 0, 1, -1};
+
+struct Wormhole{
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+}wormhole[5];
 
 
-char set_direction(char direction, int current){
-    if(current == 0) return direction;
-    else if(current == -1) return 'q';
-    else if(current == 1)
 
-    return 't';
+int search(int x, int y, int direction, int score){
+    int new_x = x;
+    int new_y = y;
+
+    while(true){
+        current = pinball[x][y] - 1;
+
+        // 블랙홀인 경우
+        if(current == -1) return score; 
+        // 자기자신으로 돌아온 경우
+        else if(start_flag == true && start_x == x && start_y == y) return score;
+        //웜홀을 만난 경우
+        else{
+            rule[current][direction];
+        }
+    }
 }
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
+    int temp;
     FILE *stream;
 
     stream = freopen("5650_sample_input.txt", "r", stdin);
@@ -45,10 +71,36 @@ int main(int argc, char** argv)
         // 1 ~ 5 블록, 6 ~ 10 웜홀(최대 5쌍)
         scanf("%d", &N);
 
+        // init wormhole
+        for(int i = 0; i < 5; i++){
+            wormhole[i].x1 = -1;
+            wormhole[i].x2 = -1;
+            wormhole[i].y1 = -1;
+            wormhole[i].y2 = -1;
+        }
+
+
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 scanf("%d", &pinball[i][j]);
+
+                //init wormhole
+                temp = pinball[i][j];
+                if(temp > 5){
+                    if(wormhole[temp - 6].x1 == -1){
+                        wormhole[temp - 6].x1 = i;
+                        wormhole[temp - 6].y1 = j;
+                    }
+                    else{
+                        wormhole[temp - 6].x2 = i;
+                        wormhole[temp - 6].y2 = j;
+                    }
+                }
             }
+        }
+
+        for(auto i: wormhole){
+            printf("(%d %d) (%d %d)\n", i.x1, i.y1, i.x2, i.y2);
         }
 
         /* 입력 확인
@@ -62,10 +114,12 @@ int main(int argc, char** argv)
 
        for(int i = 0; i < N; i++){
            for(int j = 0; j < N; j++){
-                direction = 'r';
-                current = pinball[i][j];
-
-                direction = set_direction(direction, current);
+                direction = '0';
+                start_flag = false;
+                start_x = i;
+                start_y = j;
+                // temp_answer = search(start_x, start_y, direction, 0);
+                // answer = answer > temp_answer ? answer : temp_answer;
            }
        }
 
