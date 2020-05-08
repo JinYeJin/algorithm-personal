92bb1f2434d16f24a8a383067dbf37c0b25b768e
JinYeJin <yeen0606@gmail.com>
2020-05-08 16:02:18 +0900

    kakao 2019 문제

---
+++ SWEA/18_kakao/징검다리 건너기.cpp
@@ -0,0 +1,43 @@
+/*
+2020-05-07 진예진
+프로그래머스 2019 카카오
+징검다리 건너기 https://programmers.co.kr/learn/courses/30/lessons/64062
+*/
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+
+using namespace std;
+
+
+int solution(vector<int> stones, int k) {
+    int answer = 0;
+    long long min_value, max_value, mid_value;
+    
+    min_value = *min_element(stones.begin(), stones.end());
+    max_value = *max_element(stones.begin(), stones.end());
+    
+    while(min_value <= max_value){
+        mid_value =  (min_value + max_value) / 2;
+        int end_check = 0; // 연속으로 몇개의 돌이 0이 되는지 체크
+
+        for(auto stone:stones){
+            if(end_check >= k) break;
+            // 돌이 0이 됨
+            if(mid_value > stone){
+                end_check++;
+            }
+            else end_check = 0;
+        }
+        
+        // 건널 수 없다.
+        if(end_check >= k ) max_value = mid_value - 1;
+        // 건널 수 있다.
+        else min_value = mid_value + 1;
+    }
+    answer = max_value;
+    printf("%d %d %d", min_value, mid_value, max_value);
+    return answer;
+}
---
+++ SWEA/18_kakao/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,48 @@
+/*
+2020-05-06 진예진
+프로그래머스 2019 카카오
+https://programmers.co.kr/learn/courses/30/lessons/64061
+*/
+
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+bool boom(int a, int b){
+    return a == b;
+}
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    int q_front = 0;
+    stack<int> basket;
+    
+    for(auto move:moves){
+        move = move - 1;
+        for(int depth = 0; depth < board.size(); depth++){
+        	if(board[depth][move] != 0){
+                // printf("hit: %d, %d: ", depth, move);
+                // 바구니가 비어있지 않으면
+                if(!basket.empty()){
+                    q_front = basket.top();
+                    // printf("front[ %d ] board[ %d ] ", q_front, board[depth][move]);
+                	if(q_front != board[depth][move]){
+                        basket.push(board[depth][move]);
+                    }
+                    else{
+                        basket.pop();
+                        answer += 2;
+                        // printf("boom");
+                    }
+                }
+                else basket.push(board[depth][move]);
+                board[depth][move] = 0;
+                // printf("\n");
+                break;
+            } 
+        }
+    }
+    return answer;
+}
