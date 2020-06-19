6b6f4add5d61f5b41f9a3196a5ff5777c3c7722a
JinYeJin <yeen0606@gmail.com>
2020-06-19 09:39:03 +0900

    solve 2617

--- S2/3/bj_2617.cpp
+++ S2/3/bj_2617.cpp
@@ -1,50 +1,89 @@
-#include <queue>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <cstring>
+/*
+2020-06-19 진예진
+[백준 2617 구슬 찾기] https://www.acmicpc.net/problem/2617
+*/
 #include <iostream>
+#include <vector>
+#define MAX_INT 101
+
 using namespace std;
-vector<int> relation[100];
-int is_visited[100], smaller_child[100], bigger_child[100];
- 
-int main() {
-    int N, M; cin >> N >> M;
-    int mid = (N + 1) / 2;
-    // 관계 설정 
-    for (int i = 0; i < M; ++i) {
-        int big, small; cin >> big >> small;
-        relation[big].push_back(small);
-    }
- 
-    //각 번호 별로 자기부터 작은 것을 센다
-    for (int i = 1; i <= N; ++i) {
-        int child_num = 0, start = i;
-        memset(is_visited, -1, sizeof(is_visited)); 
- 
-        // BFS 시작
-        queue<int> que;     que.push(start);
-        while (!que.empty()) {
-            int now_node = que.front(); que.pop();
-            int small_node_num = relation[now_node].size();
-            for (int j = 0; j < small_node_num; ++j) {
-                int child_node = relation[now_node][j]; 
-                // 자신보다 큰 노드의 개수를 1 추가하고
-                // 처음 BFS를 시작한 노드의 자식수 cnt 를 1 추가
-                if (is_visited[child_node] == -1) {
-                    bigger_child[child_node]++, child_num++;
-                    is_visited[child_node] = 1;
-                    que.push(child_node);
-                }
-            }
+
+
+int N, M, a, b;
+int cnt, rcnt, result, base_num;
+bool check[MAX_INT], rcheck[MAX_INT];
+
+vector<int> v[MAX_INT];// 정방향
+vector<int> r[MAX_INT];// 역방향
+
+// 정방향 dfs
+void dfs(int node){
+    // 방문한 노드를 trure로
+    check[node] = true;
+    
+    for(int i = 0; i < v[node].size(); i++){
+        int next = v[node][i];
+        // 방문한 적이 없으면 dfs로 들어감
+        if(!check[next]){
+            // ??
+            cnt++;
+            dfs(next);
         }
-        smaller_child[i] = child_num;  //자기보다 작은노드의 수 기록
     }
- 
-    int ans = 0;
-    for (int i = 1; i <= N; ++i)
-        if (bigger_child[i] >= mid || smaller_child[i] >= mid) ans++;
- 
-    cout << ans << endl;
-    return 0;
+}
+
+// 역방향 dfs
+void rdfs(int node){
+    rcheck[node] = true;
+    
+    for(int i=0; i<r[node].size(); i++){
+        int next = r[node][i];
+        if(!rcheck[next]){
+            rcnt++;
+            rdfs(next);
+        }
+    }
+}
+
+// 방문 기록 및 방문 개수 초기화
+void init(){
+    cnt = 0;
+    rcnt = 0;
+    for(int i = 1; i <= N; i++){
+        check[i] = false;
+        rcheck[i] = false;
+    }
+}
+
+int main(){
+    
+    scanf("%d %d", &N, &M);
+    
+    // 구슬이 어떻게 연결되어 있는지(엣지) 정보를 입력받는다.
+    for(int i = 0; i < M; i++){
+        scanf("%d %d", &a, &b);
+        // 정방향은 v에 넣는다(무거운거 - > 가벼운거)
+        v[a].push_back(b);
+        
+        // 역방향은 r에 넣는다.(가벼운거 -> 무거운 거) 
+        r[b].push_back(a);
+    }
+    
+    // N개 중에서 어디가 중간인지 계산
+    base_num = (N+1)/2;
+
+    // 정점마다 DFS 수행
+    for(int i=1; i<=N; i++){
+        init();
+        
+        dfs(i);
+        rdfs(i);
+        
+        // base_num 이 중간번호를 저장하고 있음, 중간번호보다 크면 땡
+        if(cnt >= base_num || rcnt >= base_num){
+            result++;
+        }
+    }
+    
+    printf("%d", result);
 }
