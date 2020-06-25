70666b56e91ee898f7407f09840fcd6b2ea25de5
JinYeJin <yeen0606@gmail.com>
2020-06-26 08:34:30 +0900

    bbb

--- S2/4/bj_2655.cpp
+++ S2/4/bj_2655.cpp
@@ -1,6 +1,7 @@
 /*
 2020-06-25 진예진
 [2655 가장높은탑쌓기] https://www.acmicpc.net/problem/2655
+참고 https://inspirit941.tistory.com/entry/Python-%EB%B0%B1%EC%A4%80-2655-%EA%B0%80%EC%9E%A5-%EB%86%92%EC%9D%80-%ED%83%91-%EC%8C%93%EA%B8%B0
 */
 #include <stdio.h>
 #include <vector>
@@ -9,26 +10,17 @@
 using namespace std;
 
 typedef struct Brick{
-    Brick(): area(0), height(0), weight(0){}
-    Brick(int area, int height, int weight): area(area), height(height), weight(weight){}
-    int area, height, weight;
+    Brick(): index(0), area(0), height(0), weight(0){}
+    Brick(int index, int area, int height, int weight): index(area), area(area), height(height), weight(weight){}
+    int index, area, height, weight;
 }Brick;
 
 bool compare(Brick &a, Brick &b){
-    if(a.area == b.area){
-        return a.height > b.weight;
-    }
-    else return a.area > b.area;
+    return a.area < b.area;
 }
 
 int N;
-int dp[101][101];
-
-bool condition_check(Brick lower, Brick upper){
-    if(lower.area < upper.area) return false; // 아랫돌이 면적이 더 작으면
-    else if(lower.weight < upper.weight) return false; // 아랫돌이 더 가벼우면
-    else return true;
-}
+int dp[101];
 
 int main(){
     FILE *stream = freopen("S2\\4\\input\\2655_input.txt", "r", stdin);
@@ -42,62 +34,45 @@ int main(){
     scanf("%d", &N);
 
     // 0 번쨰 초기화
-    bricks.push_back(Brick(0,0,0));
+    bricks.push_back(Brick(0,0,0,0));
 
-    for(int i = 0; i < N; i++){
+    for(int i = 1; i <= N; i++){
         Brick brick;
+        brick.index = i;
         scanf("%d %d %d", &brick.area, &brick.height, &brick.weight);
         bricks.push_back(brick);
     }
 
-    // 1. 너비가 넓은 것, 무게가 무거운 것 순
+    // 너비가 넓은 순
     sort(bricks.begin(), bricks.end(), compare);
 
+    
+    for(int i = 1; i <= N; i++) dp[i] = 0;
 
     for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= N; j++){
-            // 같은경우 바로패스
-            if(i == j){
-                dp[i][i] = -1;
-                continue;
-            }
-
-            Brick *lower = &bricks[i];
-            Brick *upper = &bricks[j];
-
-            // 쌓을 수 있는 조건 만족 못하면 -1
-            if(condition_check(*lower, *upper) == false){
-                dp[i][j] = -1; 
-                continue;
-            }
-
-            if(i == 1 || j == 1){
-                dp[i][j] = lower -> height + upper -> height;
-            }
-            else{
-                int temp;
-                if(dp[i][j-1] > dp[i-1][j]){
-                    temp = dp[i][j-1];
-                    if(dp[i][j-1] != -1) selected.push_back(i);
-                }
-                else{
-                    temp = dp[i-1][j];
-                    if(dp[i-1][j] != -1) selected.push_back(i-1);
-                }
-                // dp[i][j] = dp[i][j] + (dp[i][j-1] > dp[i-1][j] ? dp[i][j-1] : dp[i-1][j]);
-                dp[i][j] = temp;
-            }
-            printf("%d", dp[i][j]);
+        for(int j = 0; j < i; j++){
+            // i 밑의 돌, j 위의 돌
+            // 무게가 더 무겁다면 쌓기 가능
+            if(bricks[i].weight > bricks[j].weight)
+                dp[i] = dp[i] > dp[j] + bricks[j].height ? dp[i] : dp[j] + bricks[j].height;
         }
-        printf("\n");
     }
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            printf("%d ", dp[i][j]);
+    int max = -1;
+    int index;
+
+    for(int i = 0; i < N; i++) max = dp[i] > max ? dp[i] : max;
+    for(int i = 0; i < N; i++)
+        if(dp[i] == max) index = i;
+
+    while(index != 0){
+        if(max == dp[index]){
+            selected.push_back(bricks[index].index);
+            max -= bricks[index].height;
         }
-        printf("\n");
+        index--;
     }
+
 
     printf("%d\n", selected.size());
     for(int i = 0; i < selected.size(); i++) printf("%d\n", selected[i]);
