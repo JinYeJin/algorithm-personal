afdd737150a23d9f16951e0837922b502d098088
JinYeJin <yeen0606@gmail.com>
2020-10-09 09:42:41 +0900

    edit 1328

--- S2/18/1328.cpp
+++ S2/18/1328.cpp
@@ -3,6 +3,7 @@
 https://ksj14.tistory.com/entry/BackJoon1328-%EA%B3%A0%EC%B8%B5-%EB%B9%8C%EB%94%A9
 */
 #include <stdio.h>
+#include <iostream>
 #define MOD 1000000007
 
 using namespace std;
@@ -11,21 +12,25 @@ int n, l, r;
 int dp[101][101][101];
 
 int main(){
-    FILE *stream =freopen("S2\\\\input\\.txt", "r", stdin);
+    FILE *stream =freopen("S2\\18\\input\\1328.txt", "r", stdin);
     if(!stream) perror("freopen");
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
     int i, j, k;
     long long int temp;
- 
-    scanf("%d %d %d", &n, &l, &r);
+
+    cin >> n >> l >> r;
  
     dp[1][1][1] = 1;
-    dp[2][2][1] = dp[2][1][2] = 1;
+    dp[2][2][1] = 1;
+    dp[2][1][2] = 1;
  
-    for (i = 3; i <= n; i++) {
+    for(int i = 3; i <= n; i++){
         dp[i][i][1] = dp[i][1][i] = 1;
-        for (j = 1; j <= n; j++) {
-            for (k = 1; k <= n; k++) {
+        for(int j = 1; j <= n; j++){
+            for(int k = 1; k <= n; k++){
                 temp = dp[i - 1][j][k];
                 temp = (temp * (i - 2)) % MOD;
                 temp = (temp + dp[i - 1][j - 1][k]) % MOD;
@@ -36,6 +41,6 @@ int main(){
         }
     }
  
-    printf("%d\n", dp[n][l][r]);
+    cout << dp[n][l][r];
     return 0;
 }
