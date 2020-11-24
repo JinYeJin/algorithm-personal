6fc32fbc19be2ad6d31448e58e02605f0fb4fc5a
JinYeJin <yeen0606@gmail.com>
2020-11-24 22:12:26 +0900

    11월 셋째주

--- S2/21/5052.cpp
+++ S2/21/5052.cpp
@@ -1,9 +1,11 @@
 /*
+[백준 5052] 전화번호 목록 acmicpc.net/problem/5052
 */
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 
 using namespace std;
@@ -20,32 +22,37 @@ int main(){
     vector<string> numbers;
 
     cin >> T;
-    cin >> N;
 
     for(int t = 0; t < T; t++){
         numbers.clear();
         // 번호 입력
+
+        cin >> N;
         for(int n = 0; n < N; n++){
             string temp;
             cin >> temp;
             numbers.push_back(temp);
         }
 
+        sort(numbers.begin(), numbers.end());
         exit_flag = false;
 
         for(int n = 0; n < N-1; n++){
             for(int i = 0; i < 10; i++){
-                string *str1 = &numbers[n];
-                string *str2 = &numbers[n+1];
+                char *char1 = &numbers[n][i];
+                char *char2 = &numbers[n+1][i];
                 // str1이 str2의 prefix인 경우
                 // str2가 str1의 prefix인 경우 => 일관성이 없다.
-                if(i >= str1->size() || i >= str2->size()){
+                if(i >= numbers[n].size() || i >= numbers[n].size()){
                     cout << "NO\n";
                     exit_flag = true;
+                    break;
                 }
 
+
+                // cout << numbers[n][1] << "\n";
                 // 다른 경우엔 다음 단어로 넘어감
-                if(str1[i] != str2[i]) break;
+                if(*char1 != *char2) break;
             }
 
             if(exit_flag) break;
